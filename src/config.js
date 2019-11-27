b556d27d0b31bb9dee0a0d341f91713b14f1af1d
Michael Kirsch <michaelskirsch@gmail.com>
2019-11-27 12:45:03 -0700

    forgot to update a config variable for heroku

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL,
+  DB_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || 'changeme'
 };
